9de82441d395dbe0b79c2466214c1b8a9cc60e54
Noriaki Sakai <noriaki.sakai.develop@gmail.com>
2015-12-01 09:58:33 +0000

    finish kadai ranking.

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,10 +1,21 @@
 class RankingController < ApplicationController
   def have
-    #@rankers = Have.group(:item_id).order('count_all desc').limit(10).count
-    @rankers = Have.group(:item_id).order('count_oitems_idl desc').limit(10).count
-
+    rankers = Have.group(:item_id).order('count_all desc').limit(10).count
+    @ranker_item_and_counts = []
+    rankers.each do | item_id, count |
+      ranker_item = Item.find(item_id)
+      # １つ目がItemインスタンス、２つ目がその合計カウント
+      @ranker_item_and_counts.push([ranker_item, count])  
+    end
   end
   
   def want
+    rankers = Want.group(:item_id).order('count_all desc').limit(10).count
+    @ranker_item_and_counts = []
+    rankers.each do | item_id, count |
+      ranker_item = Item.find(item_id)
+      # １つ目がItemインスタンス、２つ目がその合計カウント
+      @ranker_item_and_counts.push([ranker_item, count])  
+    end
   end
 end
