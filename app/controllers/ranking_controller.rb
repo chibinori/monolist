c3f04f1eb1fac1772714c7fd82737990a7321cfe
Noriaki Sakai <noriaki.sakai.develop@gmail.com>
2015-12-01 05:58:55 +0000

    ranking Controller created

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,5 +1,8 @@
 class RankingController < ApplicationController
   def have
+    #@rankers = Have.group(:item_id).order('count_all desc').limit(10).count
+    @rankers = Have.group(:item_id).order('count_oitems_idl desc').limit(10).count
+
   end
   
   def want
--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,15 @@ Rails.application.routes.draw do
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  
+  get 'ranking/have'
+  get 'ranking/want'
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :relationships, only: [:create, :destroy]
   resources :ownerships, only: [:create, :destroy]
   resources :items , only: [:new , :show]
-  resource :ranking, only: [] do 
-    member do
-      get 'have', to: 'ranking#have'
-      get 'want', to: 'ranking#want'
-    end
-  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
